64822dbdf4f36d12574dc512319d8be5311ceb7d
nancom20 <73320436+nancom20@users.noreply.github.com>
2021-07-02 15:35:54 +0900

    계정

--- commands/account.py
+++ commands/account.py
@@ -8,23 +8,26 @@ class games(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    
+
     @commands.command(name= '가입')
     async def account(self, ctx):
         # user_tag = ctx.author.discriminator
         user_id = ctx.author.id
         user_name = ctx.author.name
+        emoji = ['🟩', '🟥']
 
         with open('user.json', 'r', encoding='utf-8') as f: user_data = json.load(f)
 
         if f'{user_id}' in user_data:
             embed = discord.Embed(color= 0xec4747)
-            embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
+            embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
             embed.add_field(name="오...", value="이미 가입 돼있네요............", inline=True)
             await ctx.channel.send(embed=embed)
 
         else:
             embed = discord.Embed(color= 0x00ff9c)
-            embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
+            embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
             embed.add_field(name="가입입", value="뭐 저장하는건 닉네임과 아이디 밖에없서요 ¯\_(ツ)_/¯", inline=True)
             embed.add_field(name="ㅁㅁㅁ", value="동의 하지 않으면 이용할떄 일부제한이 있을수도있서요", inline=False)
@@ -33,53 +36,45 @@ class games(commands.Cog):
             await msg.add_reaction('🟥')
 
             def check(reactin, user):
-                return user == ctx.message.author and str(reactin.emoji) == '🟩'
-
+                return str(reactin) in emoji and user == ctx.author and reactin.message.id == msg.id
             try:
-                reactin, user = await self.bot.wait_for(event='reaction_add', timeout=15.0, check=check)
-
+                reactin, _user = await self.bot.wait_for(event='reaction_add', timeout=15.0, check=check)
             except asyncio.TimeoutError:
                 embed = discord.Embed(color= 0x00ff9c)
-                embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
+                embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
                 embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
                 embed.add_field(name="어음.........", value="흠ㅁ흐으음", inline=True)
                 await msg.edit(embed=embed)
-
             else:
-    
-                Information = OrderedDict()
-                user_Information = OrderedDict()
-                user_Information["name"] = user_name
-                user_Information["money"] = 100
-                user_Information["fish"] = {'salmon': 0, 'Mackerel': 0, 'tuna': 0, 'cod': 0, 'Clownfish': 0, 'goldfish': 0}
-                user_Information["Inventory"] = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
-                Information[user_id] = user_Information
-                user_data.update(Information)
+                if str(reactin) == '🟩':
+                    Information = OrderedDict()
+                    user_Information = OrderedDict()
+                    user_Information["name"] = user_name
+                    user_Information["money"] = 100
+                    user_Information["fish"] = {'salmon': 0, 'Mackerel': 0, 'tuna': 0, 'cod': 0, 'Clownfish': 0, 'goldfish': 0}
+                    user_Information["Inventory"] = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+                    Information[user_id] = user_Information
+                    user_data.update(Information)
 
-                with open('user.json', 'w', encoding='utf-8') as outfile: json.dump(user_data , outfile, indent=4)
+                    with open('user.json', 'w', encoding='utf-8') as outfile: json.dump(user_data , outfile, indent=4)
         
-                embed = discord.Embed(color= 0x00ff9c)
-                embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
-                embed.add_field(name="우와와아우", value="가입 완료!", inline=True)
-                await msg.edit(embed=embed)
-
-        # def check(reactin, user):
-        #     return user == ctx.message.author and str(reactin.emoji) == '🟥'
-        # try:
-        #     reactin, user = await self.bot.wait_for(event='reaction_add', timeout=60.0, check=check)
-        # except asyncio.TimeoutError:
-        #     await ctx.channel.send("어음.....")
-        # else:
-        #     embed = discord.Embed(color= 0x00ff9c)
-        #     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
-        #     embed.add_field(name="파이", value="가입 하지 않으면 이용할떄 제약이 있서요ㅠ", inline=True)
-        #     await msg.edit(embed=embed)
+                    embed = discord.Embed(color= 0x00ff9c)
+                    embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
+                    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
+                    embed.add_field(name="우와와아우", value="가입 완료!", inline=True)
+                    await msg.edit(embed=embed)    
+                elif str(reactin) == '🟥':
+                    embed = discord.Embed(color= 0x00ff9c)
+                    embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
+                    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
+                    embed.add_field(name="파이", value="ㅠ", inline=True)
+                    await msg.edit(embed=embed)
 
     @commands.command(name= '탈퇴')
     async def secession(self, ctx):
         user_id = ctx.author.id
         user_name = ctx.author.name
+        emoji = ['🟩', '🟥']
 
         with open('user.json', 'r', encoding='utf-8') as f: user_data = json.load(f)
 
@@ -92,10 +87,10 @@ class games(commands.Cog):
             await msg.add_reaction('🟥')
 
             def check(reactin, user):
-                return user == ctx.message.author and str(reactin.emoji) == '🟩'
+                return str(reactin) in emoji and user == ctx.author and reactin.message.id == msg.id
 
             try:
-                reactin, user = await self.bot.wait_for(event='reaction_add', timeout=15.0, check=check)
+                reactin, _user = await self.bot.wait_for(event='reaction_add', timeout=15.0, check=check)
 
             except asyncio.TimeoutError:
                 embed = discord.Embed(color= 0x00ff9c)
@@ -105,13 +100,19 @@ class games(commands.Cog):
                 await msg.edit(embed=embed)
 
             else:
-                embed = discord.Embed(color= 0x00ff9c)
-                embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
-                embed.add_field(name="파이", value="성공적으로 탈퇴했서요 언젠간 다시 와요 ㅠ", inline=True)
-                await msg.edit(embed=embed)
+                if str(reactin) == '🟩':
+                    embed = discord.Embed(color= 0x00ff9c)
+                    embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
+                    embed.add_field(name="파이", value="성공적으로 탈퇴했서요 언젠간 다시 와요 ㅠ", inline=True)
+                    await msg.edit(embed=embed)
 
-                del user_data[f"{user_id}"]
-                with open('user.json', 'w', encoding='utf-8') as outfile: json.dump(user_data , outfile, indent=4)
+                    del user_data[f"{user_id}"]
+                    with open('user.json', 'w', encoding='utf-8') as outfile: json.dump(user_data , outfile, indent=4)
+                elif str(reactin) == '🟥':
+                    embed = discord.Embed(color= 0x00ff9c)
+                    embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
+                    embed.add_field(name="ㅠ", value=":sob:", inline=True)
+                    await msg.edit(embed=embed)
 
         else:
             embed = discord.Embed(color= 0xec4747)
