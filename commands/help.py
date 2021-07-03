b8b4ba216f9b39a9061b9c550f6060d44f389ad1
citc-walker <75872281+citc-walker@users.noreply.github.com>
2021-07-03 12:53:52 +0900

    help.py 수정
    맞춤법 안맞는거 수정함

--- commands/help.py
+++ commands/help.py
@@ -16,8 +16,8 @@ class Core(commands.Cog):
         embed = discord.Embed(title = "희망봇",color= 0x00ff9c)
         embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png",)
         embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
-        embed.add_field(name="희망위키", value="다양한 정보를 보거나 추가할수 있서요 \n ``위키만들기``:위키를 새로 만들어요 \n ``페이지추가``:위키에 페이지를 추가해요 \n ``위키삭제``:위키를 삭제해요 \n ``페이지삭제``:페이지를 삭제해요", inline=True)
-        embed.add_field(name="게임", value="``가입``:희망봇에 가입을해요 \n ``탈퇴``:희망봇에 탈퇴해요 \n ``낚시``:물고기를 낚아요 \n ``낚시상점``: 낚시와 관련된 아이템을 사거나 팔수 있서요\n ``인벤토리``: 인벤토리를 봐요", inline=True)
+        embed.add_field(name="희망위키", value="다양한 정보를 보거나 추가할 수 있아요 \n ``위키만들기``:위키를 새로 만들어요 \n ``페이지추가``:위키에 페이지를 추가해요 \n ``위키삭제``:위키를 삭제해요 \n ``페이지삭제``:페이지를 삭제해요", inline=True)
+        embed.add_field(name="게임", value="``가입``:희망봇에 가입을 해요 \n ``탈퇴``:희망봇에 탈퇴를 해요 \n ``낚시``:물고기를 낚아요 \n ``낚시상점``: 낚시와 관련된 아이템을 사거나 팔 수 있서요\n ``인벤토리``: 인벤토리를 봐요", inline=True)
         embed.set_footer(icon_url=ctx.author.avatar_url ,text=f"{user_name}의해 실행됨")
 
         await ctx.channel.send(embed=embed)
