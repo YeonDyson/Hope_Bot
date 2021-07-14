9ae2990556674ca7e74005ed04446267b904fe02
citc-walker <75872281+citc-walker@users.noreply.github.com>
2021-07-14 22:21:36 +0900

    맞춤법수정
    맞춤법 수정함

--- commands/account.py
+++ commands/account.py
@@ -27,8 +27,8 @@ class games(commands.Cog):
             embed = discord.Embed(color= 0xfbb907)
             embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
             embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
-            embed.add_field(name="가입입", value="뭐 저장하는건 닉네임과 아이디 밖에없서요 ¯\_(ツ)_/¯", inline=True)
-            embed.add_field(name="ㅁㅁㅁ", value="동의 하지 않으면 이용할떄 일부제한이 있을수도있서요", inline=False)
+            embed.add_field(name="가입입", value="뭐 저장하는건 닉네임과 아이디 밖에 없어요 ¯\_(ツ)_/¯", inline=True)
+            embed.add_field(name="ㅁㅁㅁ", value="동의하지 않으면 이용할때 일부제한이 있을 수도 있어요", inline=False)
             msg = await ctx.channel.send(embed=embed)
             await msg.add_reaction('🟩')
             await msg.add_reaction('🟥')
@@ -41,7 +41,7 @@ class games(commands.Cog):
                 embed = discord.Embed(color= 0xfbb907)
                 embed.set_author(name="희망봇", icon_url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
                 embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/773727937069056000/857254590218371082/526_B39FCE5.png")
-                embed.add_field(name="어음.........", value="흠ㅁ흐으음", inline=True)
+                embed.add_field(name="어음.........", value="흠..흐으음", inline=True)
                 await msg.edit(embed=embed)
             else:
                 if str(reactin) == '🟩':
